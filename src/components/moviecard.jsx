48c7ea1310e410039077c18c72ef3f0575e0d7e4
Dhanvin⚡️ <Dhanvin@Dhanvins-MacBook-Pro.local>
2025-05-04 14:36:34 +0530

    sdsd

---
+++ src/components/moviecard.jsx
@@ -0,0 +1,43 @@
+import '../css/MovieCard.css'
+import { useMovieContext } from '../contexts/MovieContext'
+
+function MovieCard({ movie }) {
+  const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext()
+  const favorite = isFavorite(movie.id)
+
+
+  const onFavoriteClick = (e) => {
+    e.preventDefault()
+    if (favorite) {
+      removeFromFavorites(movie.id)
+    } else {
+      addToFavorites(movie)
+    }
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster-container">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+          className="movie-poster"
+        />
+        <div className="movie-overlay">
+          <button
+            className={`favorite-btn ${favorite ? 'active' : ''}`}
+            onClick={onFavoriteClick}
+          >
+            ♥
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3 className="movie-title">{movie.title}</h3>
+        <p className="movie-release-year">{movie.release_date?.split('-')[0]}</p>
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
