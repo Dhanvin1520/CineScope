05c98258e5f81dab3b2e2b44c99d18559bfe6d29
Dhanvin1520 <dhanvin.vadlamudi269@gmail.com>
2025-06-23 13:18:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import './css/App.css';
+
 import Home from './pages/home';
 import Favorites from './pages/favorites';
 import LoginPage from './pages/login';
+
 import NavBar from './components/navbar';
+
 import { MovieProvider } from './contexts/MovieContext';
+import { AuthProvider, useAuth } from './contexts/AuthContext';
 
 function App() {
   return (
-    <MovieProvider>
-      <NavBar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/login" element={<LoginPage />} /> 
-        </Routes>
-      </main>
-    </MovieProvider>
+    <AuthProvider>
+      <MovieProvider>
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/favorites"
+              element={
+                <ProtectedRoute>
+                  <Favorites />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </main>
+      </MovieProvider>
+    </AuthProvider>
   );
 }
 
 export default App;
+
+
+function ProtectedRoute({ children }) {
+  const { user } = useAuth();
+  const location = useLocation();
+
+  if (!user) {
+
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
